d04db60dcffdcc34acee39c4bc6c3d8727f3a4c9
Mark Pfluger <mark@pfluegerworld.de>
2016-03-11 11:48:46 -0500

    OkClient works with interface changes

--- src/main/java/info/blockchain/api/OkClient.java
+++ src/main/java/info/blockchain/api/OkClient.java
@@ -3,6 +3,7 @@ package info.blockchain.api;
 import com.squareup.okhttp.*;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
@@ -56,30 +57,67 @@ public class OkClient implements HttpClientInterface {
      * This method makes a get request to a specific API resource.
      * Request is built using the OkHttp {@link HttpUrl} builder.
      *
+     * @param baseURL  The API base URL
      * @param resource The API resource being requested.
      * @param params   A set of params sent to the resource
      * @return Returns a response as a {@link String}
      * @throws IOException Thrown if the request was unsuccessful.
      */
     @Override
-    public String get(String resource, Map<String, String> params) throws APIException, IOException {
+    public String get(String baseURL, String resource, Map<String, String> params) throws IOException {
+        HttpUrl.Builder url = getHttpUrlBuilder(baseURL, resource);
 
+        for (String paramName : params.keySet()) {
+            url.addEncodedQueryParameter(paramName, params.get(paramName));
+        }
+
+        return getInternal(url.build());
+    }
+
+    /**
+     * This method makes a get request to a specific API resource.
+     * Request is built using the OkHttp {@link HttpUrl} builder.
+     *
+     * @param resource The API resource being requested.
+     * @param params   A set of params sent to the resource
+     * @return Returns a response as a {@link String}
+     * @throws IOException Thrown if the request was unsuccessful.
+     */
+    @Override
+    public String get(String resource, Map<String, String> params) throws IOException {
         HttpUrl.Builder url = getHttpUrlBuilder(resource);
 
         for (String paramName : params.keySet()) {
             url.addEncodedQueryParameter(paramName, params.get(paramName));
         }
 
+        return getInternal(url.build());
+    }
+
+    private String getInternal(HttpUrl url) throws IOException {
         Request request = new Request.Builder()
-                .url(url.build())
+                .url(url)
                 .build();
 
-        Response response = getOkHttpClientInstance().newCall(request).execute();
-        if (isNotSuccessfulResponse(response))
-            throw new IOException(String.format("Unsuccessful call to %s Response: %s",
-                    request.urlString(),
-                    response));
-        return response.body().string();
+        return makeRequest(request);
+    }
+
+    /**
+     * This method makes a post request to a specific API resource.
+     * Request is built using the OkHttp {@link HttpUrl} builder.
+     * Request body is built using the OkHttp {@link FormEncodingBuilder} builder .
+     *
+     * @param baseURL  The API base URL
+     * @param resource The API resource being requested.
+     * @param params   A set of params sent to the resource
+     * @return Returns a response as a {@link String}
+     * @throws IOException Thrown if the request was unsuccessful.
+     */
+    @Override
+    public String post(String baseURL, String resource, Map<String, String> params) throws IOException {
+          HttpUrl.Builder url = getHttpUrlBuilder(baseURL, resource);
+
+          return postInternal(url.build(), params);
     }
 
     /**
@@ -93,28 +131,34 @@ public class OkClient implements HttpClientInterface {
      * @throws IOException Thrown if the request was unsuccessful.
      */
     @Override
-    public String post(String resource, Map<String, String> params) throws APIException, IOException {
-
+    public String post(String resource, Map<String, String> params) throws IOException {
         HttpUrl.Builder url = getHttpUrlBuilder(resource);
 
+        return postInternal(url.build(), params);
+    }
+
+    private String postInternal(HttpUrl url, Map<String, String> params) throws IOException {
         FormEncodingBuilder formEncodingBuilder = new FormEncodingBuilder();
         for (String paramName : params.keySet()) {
             formEncodingBuilder.addEncoded(paramName, params.get(paramName));
         }
 
         Request request = new Request.Builder()
-                .url(url.build())
+                .url(url)
                 .post(formEncodingBuilder.build())
                 .build();
 
-        Response response = getOkHttpClientInstance().newCall(request).execute();
-        if (isNotSuccessfulResponse(response))
-            throw new IOException(String.format("Unsuccessful call to %s Response: %s",
-                    request.urlString(),
-                    response));
+        return makeRequest(request);
+  }
 
-        return response.body().string();
+    private String makeRequest(Request request) throws IOException {
+      Response response = getOkHttpClientInstance().newCall(request).execute();
+      if (isNotSuccessfulResponse(response))
+          throw new IOException(String.format("Unsuccessful call to %s Response: %s",
+                  request.urlString(),
+                  response));
 
+      return response.body().string();
     }
 
     private HttpUrl.Builder getHttpUrlBuilder(String resource) {
@@ -122,7 +166,20 @@ public class OkClient implements HttpClientInterface {
         url.scheme(URL_SCHEME)
                 .host(URL_HOST)
                 .addPathSegment(resource);
+
         return url;
+    }
+
+    private HttpUrl.Builder getHttpUrlBuilder(String baseURL, String resource) throws MalformedURLException {
+        HttpUrl url = HttpUrl.parse(baseURL);
+        if (url == null) {
+          throw new MalformedURLException();
+        }
+
+        HttpUrl.Builder urlBuilder = url.newBuilder();
+        urlBuilder.addPathSegment(resource);
+
+        return urlBuilder;
     }
 
     private boolean isNotSuccessfulResponse(Response response) {
